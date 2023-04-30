6e1592d9eee1116ed347102d62e3bdedb65885b0
Daniel DeFreez <defreezd@sou.edu>
2023-04-30 15:42:10 -0700

    first commit

---
+++ desktop/src/edu/sou/cs452/drop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package edu.sou.cs452.drop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+   public static void main (String[] arg) {
+      Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+      config.setTitle("Drop");
+      config.setWindowedMode(800, 480);
+      config.useVsync(true);
+      config.setForegroundFPS(60);
+      new Lwjgl3Application(new Drop(), config);
+   }
+}
